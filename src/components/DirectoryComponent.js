5fd0764d0ded779f5eb745cf14aadfc6143715c9
Ren Santiago <82529907+mashmerro@users.noreply.github.com>
2021-10-07 15:07:26 -0500

    DirectoryComponent.js comments

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react';
+// src/components folder will be custom components we will make
+// This file is a module because there's an export
+// Since this file is a React component, it MUST return a React element
+// images from: public/assets/images
+import React, { Component } from 'react';   // Default React import, { named import called 'Component' }
 
-class Directory extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+class Directory extends Component {     // create a child class (Directory) from parent class (Component) that we imported on line 3
+    constructor(props) {     // 'props' : keyword in React | constructor is not always required
+        super(props);        // 'this.props' is already in the 'Component' parent class so use 'super()'
+        this.state = {       // 'state' : property in React that always holds an object. 'campsites' is its property with diff arr
             campsites: [
                 {
                     id: 0,
@@ -13,11 +17,11 @@ class Directory extends Component {
                     description: "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers."
                 },
                 {
-                  id: 1,
-                  name: 'Chrome River Campground ',
-                  image: 'assets/images/chrome-river.jpg',
-                  elevation: 877,
-                  description: "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River."
+                    id: 1,
+                    name: 'Chrome River Campground ',
+                    image: 'assets/images/chrome-river.jpg',
+                    elevation: 877,
+                    description: "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River."
                 },
                 {
                     id: 2,
@@ -37,24 +41,32 @@ class Directory extends Component {
         };
     }
 
-    render() {
+    render() {  // must have 1 return statement
+        // 'directory' : variable that will return an array of elements
+        //  grab the array from 'this.state.campsites' | .map() will return a new array 
         const directory = this.state.campsites.map(campsite => {
-            return (
-                <div key={campsite.id} className="col">
-                    <img src={campsite.image} alt={campsite.name} />
+            return (    // Notice: This return is FROM THIS ARROW FUNCTION NOT THE WHOLE COMPONENT
+                        // Since we're rendering an array of elements, you MUST ADD A 'key' attribute to the parent class (this usually represents an ID {this.state.campsites.id})
+                <div key={campsite.id} className="col"> 
+                    <img src={campsite.image} alt={campsites.name} />
                     <h2>{campsite.name}</h2>
                     <p>{campsite.description}</p>
                 </div>
+                // This is all JSX, so if we want to call something, use {}
             );
-        });
-        return (
-            <div className="container">
-                <div className="row">
-                    {directory}
+        });     
+        return (        // Notice: returns 1 element (div=container parent class) and its children
+                        // 'className' for React js instead of 'class' in HTML
+                        // { javascript variable (line 45) is getting called (or rendered in this case) } must have curly braces inside of JSX
+            <div className="container">     
+                <div className="row">       
+                    {directory}             
                 </div>
             </div>
         );
     }
 }
 
-export default Directory;
+export default Directory;   // go to 'App.js' then add:  '  import Directory from './components/DirectoryComponent';   '
+                            //      > still on 'App.js', render it in the return statement of 'render()' with its 
+                            //         class name 'Directory' (line 7) by  ' <Directory /> '
